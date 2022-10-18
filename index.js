6ce92f950ec7eae446c2e810fa66da4b70eb21d6
RstTiwari <iamrrt2121@gmail.com>
2022-10-18 22:31:41 +0530

    added env

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const userroute = require("./app/user");
-const creatUser = require("./app/create");
+const dotenv = require("dotenv")
+const userroute = require("./app/controller/create");
+const creatUser = require("./app/controller/alluser");
+dotenv.config()
 
 const app = express();
 app.use(express.json());
@@ -13,7 +15,7 @@ app.use("/api", creatUser);
 
 mongoose
   .connect(
-    "mongodb://myfac8ry:myfac8ry@ac-z992smf-shard-00-00.fmmrhks.mongodb.net:27017,ac-z992smf-shard-00-01.fmmrhks.mongodb.net:27017,ac-z992smf-shard-00-02.fmmrhks.mongodb.net:27017/?ssl=true&replicaSet=atlas-piyukq-shard-0&authSource=admin&retryWrites=true&w=majority",
+    process.env.DB_URL,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() =>
