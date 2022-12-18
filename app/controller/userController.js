aa73425dfdaa9d726750602f4ff2c081b6546be9
RstTiwari <iamrrt2121@gmail.com>
2022-12-18 23:14:51 +0530

    added cloud function

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -7,7 +7,7 @@ const saltRounds = 10;
 const User = require("../modal/userModel");
 const userService = require("../services/userServices");
 const bycrypt = require("bcrypt");
-const helper = require("../helper/numberFun")
+const  { google } = require("googleapis") 
 
 
 
@@ -107,10 +107,14 @@ const userController = {
   },
   sendFile: async function (req, res) {
        try {
+        if (!req.file) {
+          res.status(400).send("No file uploaded.");
+          return;
+        }
 
       // number defined for setting enquiryId
       let counterData = await userService.getCounter("enquiry")
-      let fileName = req.file.originalname;
+      let fileName = `${counterData.counterSeq}/${req.file.originalname}`;
 
       // now saving the data on db
       // checking if file is alredy there
@@ -121,41 +125,50 @@ const userController = {
       }
       let obj = {
         enquiryId: counterData.counterSeq,
-        fileName: `${fileName}`,
-      };
-      let enquiryData = await userService.enquiryFile(obj);
-      if (enquiryData.length < 1) throw new Error("File could not be saved")
-      // sending us mail of enquiry
-      let tranporter = nodeMailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user:"info.myfac8ry@gmail.com",
-          pass:"qanbmwhykokzjxut"
-
-        },
-      });
-
-      let info = await tranporter.sendMail({
-        from: "info.myfac8ry@gmail.com",
-        to: "info.myfac8ry@gmail.com",
-        subject: " myfac8ry enquiry  Mail",
-        text: "Recived an enquiry mail",
-        attachments: [
-          {
-            path: `enquiry/${fileName}`, 
-          },
-        ],
-      });
-      if(info.errno){
-        throw new Error("Failed to send email")
+        fileName: fileName,
       }
 
-        res.send({
-          status: 1,
-          message: "Enquiry created Succesfull !!",
+      const auth = userController.authenticateGoogle();
+      const response = await userController.uploadToGoogleDrive(req.file,fileName, auth);
+      if(response.success = 200){
+        let enquiryData = await userService.enquiryFile(obj);
+        if (enquiryData.length < 1) throw new Error("File could not be saved")
+        // sending us mail of enquiry
+        let tranporter = nodeMailer.createTransport({
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+            user:"info.myfac8ry@gmail.com",
+            pass:"qanbmwhykokzjxut"
+  
+          },
         });
+  
+        let info = await tranporter.sendMail({
+          from: "info.myfac8ry@gmail.com",
+          to: "info.myfac8ry@gmail.com",
+          subject: " myfac8ry enquiry  Mail",
+          text: "Recived an enquiry mail",
+          attachments: [
+            {
+              path:req.file.path, 
+            },
+          ],
+        });
+        if(info.response = "250 2.0.0 OK  1671384732 c12-20020a170903234c00b0017ec1b1bf9fsm5325160plh.217 - gsmtp"){
+          res.send({
+            success: 1,
+            message: "Enquiry created Succesfull !!",
+          });
+        }
+        
+
+        userController.deleteFile(req.file.path);
+
+      }else{
+        throw "File could not be uploaded"
+      }
     } catch (error) {
       console.log("Error in parsing file", error);
       return res.send({
@@ -164,5 +177,38 @@ const userController = {
       });
     }
   },
+   authenticateGoogle :() => {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: `/home/rstpersonal/myfac8ry/myfac8ryapi/myfac8ry.json`,
+      scopes: "https://www.googleapis.com/auth/drive",
+    });
+    return auth;
+  },
+  uploadToGoogleDrive : async (file,fileName, auth) => {
+    const fileMetadata = {
+      name: fileName,
+      parents: ["11TrC4SBNy62qxd0wTDGxHr8391e9isHU"], // Change it according to your desired parent folder id
+    };
+  
+    const media = {
+      mimeType: file.mimetype,
+      body: fs.createReadStream(file.path),
+    };
+  
+    const driveService = google.drive({ version: "v3", auth });
+  
+    const response = await driveService.files.create({
+      requestBody: fileMetadata,
+      media: media,
+      fields: "id",
+    });
+    return response;
+  },
+  deleteFile:(filePath) => {
+    fs.unlink(filePath, () => {
+      console.log("file deleted");
+    });
+  }
+
 };
 module.exports = userController;
